b00ca9aae85731d29fe179f40314509b8662d84b
Ben Beacham <deadpangameryt@gmail.com>
2025-08-04 15:57:04 +0100

    Update header in Home component of page.tsx from "Update 1" to "Update 2".

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Update 1</h1>
+        <h1 className="text-4xl font-bold">Update 2</h1>
       </div>
     </main>
   );
