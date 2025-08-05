dec85a6fe491292ed7a3f837ffe1e8800cbf595c
deadpandev <deadpangameryt@gmail.com>
2025-08-05 01:36:21 +0100

    Update layout.tsx metadata for project title and description, and remove unused floating-nav component to streamline codebase.

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@ import Hero from "@/components/hero";
 import Project from "@/components/project";
 import Testimonials from "@/components/testistimonials";
 import Contact from "@/components/contact";
-import { FloatingNav } from "@/components/ui/floating-nav";
-import { navItems } from "@/data";
 import About from "@/components/about";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center w-full bg-slate-950">
-        <FloatingNav navItems={navItems} />
         <Hero />
         <About />
         <Project />
